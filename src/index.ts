a8581b9a4580e464ee7f624891e1f5439667869d
Dayvid Albuquerque <kkd4yv1d@gmail.com>
2025-06-02 01:44:40 -0300

    update

--- src/index.ts
+++ src/index.ts
@@ -4,93 +4,105 @@ import { Text } from "./elements.js";
 
 /** @throws {Error} */
 export function renderToString<T>(
-  instance: ComposableInstance<T>
+    instance: ComposableInstance<T>
 ): string {
-  if (!instance) {
-    throw new Error("There is no instance to render into the element");
-  }
+    if (!instance) {
+        throw new Error("There is no instance to render into the element");
+    }
 
-  function doRender<T>(instance: ComposableInstance<T>) {
-    instance.resetRememberedStateRefCounter();
+    function doRender<T>(instance: ComposableInstance<T>) {
+        instance.resetRememberedStateRefCounter();
 
-    let children = [];
+        let children = [];
 
-    if (instance.rawChildren) {
-      for (let [index, child] of instance.rawChildren.entries()) {
-        if (typeof (child) === 'undefined')
-          continue;
+        if (instance.rawChildren) {
+            for (let [_, child] of instance.rawChildren.entries()) {
+                if (typeof (child) === 'undefined')
+                    continue;
 
-        if (typeof(child) === 'string') {
-          child = Text(child);
+                if (typeof (child) === 'string') {
+                    child = Text(child);
+                }
+
+                const result = doRender(child);
+
+                children.push(result);
+            }
         }
 
-        const result = doRender(child);
+        instance.setChildren(children);
 
-        children.push(result);
-      }
+        let result = instance.information.render(instance);
+
+        while (result instanceof ComposableInstance) {
+            result = doRender(result);
+        }
+
+        return result;
     }
 
-    instance.setChildren(children);
+    function escapeHTML(str: string) {
+        return str.replace(/[&<>"']/g, match => {
+            const entities = {
+                '&': '&amp;',
+                '<': '&lt;',
+                '>': '&gt;',
+                '"': '&quot;',
+                "'": '&#039;'
+            };
 
-    let result = instance.information.render(instance);
-
-    while (result instanceof ComposableInstance) {
-      result = doRender(result);
+            return entities[match as keyof typeof entities];
+        });
     }
 
-    return result;
-  }
+    function elementToString(element: Element): string {
+        let result = ``;
 
-  function escapeHTML(str: string) {
-    return str.replace(/[&<>"']/g, match => {
-      const entities = {
-        '&': '&amp;',
-        '<': '&lt;',
-        '>': '&gt;',
-        '"': '&quot;',
-        "'": '&#039;'
-      };
+        if (!element) return result;
 
-      return entities[match as keyof typeof entities];
-    });
-  }
+        if (element.tagName == 'html') {
+            result += '<!DOCTYPE html>';
+        }
 
-  function elementToString(element: Element): string {
-    let result = ``;
+        if (element.tagName == '#text') {
+            return escapeHTML((element as TextElement).children[0]);
+        }
 
-    if (element.tagName == 'html') {
-      result += '<!DOCTYPE html>';
+        if (element.tagName == '#raw-html') {
+            return (element as RawHTMLElement).children[0];
+        }
+
+        result += `<${element.tagName}`;
+
+        if (element.attributes && Array.isArray(element.attributes)) {
+            for (let attribute of element.attributes) {
+                if (attribute.code) {
+                    console.warn("Code attribute not handled: ", attribute.key);
+
+                    continue;
+                }
+
+                result += ` ${attribute.key}`;
+
+                if (attribute.value && typeof (attribute.value) != 'boolean' && attribute.value !== true) {
+                    result += `="${attribute.value}"`
+                }
+            }
+        }
+
+        result = result.trim() + `>`;
+
+        if (element.children && Array.isArray(element.children)) {
+            for (let child of element.children) {
+                result += elementToString(child);
+            }
+        }
+
+        if (element.needCloseTag)
+            result += `</${element.tagName}>`;
+
+        return result;
     }
 
-    if (element.tagName == '#text') {
-      return escapeHTML((element as TextElement).children[0]);
-    }
-
-    if (element.tagName == '#raw-html') {
-      return (element as RawHTMLElement).children[0];
-    }
-
-    result += `<${element.tagName}`;
-
-    for (let attribute of element.attributes) {
-      result += ` ${attribute.key}`;
-
-      if (attribute.value && attribute.value !== true) {
-        result += `="${attribute.value}"`
-      }
-    }
-
-    result = result.trim() + `>`;
-
-    for (let child of element.children) {
-      result += elementToString(child);
-    }
-
-    if (element.needCloseTag)
-      result += `</${element.tagName}>`;
-
-    return result;
-  }
-
-  return elementToString(doRender(instance));
+    return elementToString(doRender(instance));
 }
